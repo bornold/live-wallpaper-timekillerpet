77d796e12519565200d088ee84c07808dcfa2dbf
amanda.stromsten@gmail.com <amanda.stromsten@gmail.com@0193a336-4227-71e7-eace-94df50081ebd>
2011-09-28 15:33:58 +0000

    teest

--- TimeKillerPet/src/dat255/chalmers/timekillerpet/TimeKillerPetActivity.java
+++ TimeKillerPet/src/dat255/chalmers/timekillerpet/TimeKillerPetActivity.java
@@ -2,12 +2,15 @@ package dat255.chalmers.timekillerpet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TimeKillerPetActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	super.onCreate(savedInstanceState);
+    	TextView tv = new TextView(this);
+    	tv.setText("Hello, TimeKillerPet");
+        setContentView(tv);
     }
 }
